af0d948a9fd0b88d1c64262e79a8b9a20fa3ef89
Orlin <orlinsperez28@gmail.com>
2020-09-13 11:38:53 -0400

    Updates

---
+++ precache-manifest.50fd1f981ea813496df1b276e6e7013c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ebd70fe7dead89cb6f2d8b6b4819c9",
+    "url": "/OrinSperez/MultisiteON.git/index.html"
+  },
+  {
+    "revision": "cec273f26a2ba0b1a718",
+    "url": "/OrinSperez/MultisiteON.git/static/css/main.efcb08e9.chunk.css"
+  },
+  {
+    "revision": "8f1e8686466d1d395fbc",
+    "url": "/OrinSperez/MultisiteON.git/static/js/2.c3cad723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/OrinSperez/MultisiteON.git/static/js/2.c3cad723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cec273f26a2ba0b1a718",
+    "url": "/OrinSperez/MultisiteON.git/static/js/main.437b58f1.chunk.js"
+  },
+  {
+    "revision": "ef3e819f5453bfbe31f3",
+    "url": "/OrinSperez/MultisiteON.git/static/js/runtime-main.8d873812.js"
+  },
+  {
+    "revision": "5ac6fe3634edc0e0ca34e876efc3c28f",
+    "url": "/OrinSperez/MultisiteON.git/static/media/Pc.5ac6fe36.png"
+  },
+  {
+    "revision": "2427c94dc94d9bc77f69d3171f22dd84",
+    "url": "/OrinSperez/MultisiteON.git/static/media/Phone.2427c94d.png"
+  },
+  {
+    "revision": "f5cbbe1dc42d376c87e20477cb3c9303",
+    "url": "/OrinSperez/MultisiteON.git/static/media/idea.f5cbbe1d.png"
+  },
+  {
+    "revision": "53b834909450a16de8448adb872c4684",
+    "url": "/OrinSperez/MultisiteON.git/static/media/nube.53b83490.png"
+  }
+]);
